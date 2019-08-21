8f6a6d4babbdbe5b414206b4192117c4cb0cd87a
mutembeijoe <mutembeijoe@gmail.com>
2019-08-21 13:44:57 +0300

    add smooth scroll behaviour using jquery

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,19 @@ $(document).ready(function(){
            $(this).prev().fadeIn();
         })
     })
-    
+    //smooth scroll behaviour in jquery
+   $('a[href="#about"]').click(function(e){
+       if(this.hash != ""){
+           e.preventDefault();
+
+           let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            },800,function(){
+                window.location.hash = hash;
+            })
+
+       }
+   })
     
 })
