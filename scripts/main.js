9aa2bd938f8a5aa8acdb1d489d72ab4a808c08d2
mutembeijoe <mutembeijoe@gmail.com>
2019-08-23 22:05:06 +0300

    add mailChimp api

--- scripts/main.js
+++ scripts/main.js
@@ -35,20 +35,49 @@ $(document).ready(function(){
        }
    })
 
-   $('form').submit(function(e){
-       let name = $('input[name="name"]').val();
-       let email = $('input[name="email"]').val();
-       let message = $('textarea').val();
-       console.log(name,email,message);
-       //send email
+//    $('form').submit(function(e){
+//        let name = $('input[name="client_name"]').val();
+//        let email = $('input[name="client_email"]').val();
+//        let message = $('textarea').val();
+//        console.log(name,email,message);
+//        //send email
 
-       //clear inputs
-       $('input[name="email"')[0].value = "";
-       $('input[name="name"')[0].value = "";
-       $('textarea')[0].value = "";
-    // success alert 
-       $('form').prepend(`<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Hi, ${name} we received your mail</div>`);
-        e.preventDefault();
-   })
+//        //clear inputs
+//        $('input[name="client_email"')[0].value = "";
+//        $('input[name="client_name"')[0].value = "";
+//        $('textarea')[0].value = "";
+//     // success alert 
+//        $('form').prepend(`<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Hi, ${name} we received your mail</div>`);
+//         e.preventDefault();
+//    })
+
+
+
+
+/*  Mailchimp Subscribe  */
+$('#subscribe-form').bind('submit', function(event) {
+    event.preventDefault(); //prevent page refresh
+    var name = $('[name="client_name"]').val();//get name field value
+    var email = $('[name="client_email"]').val();//get email field value
+    var message = $('[name="client_message"]').val();//get email field value
+    var url =   "https://us3.api.mailchimp.com/2.0/lists/subscribe.json?"
+               +  "apikey=ea85a83ce65e7bc74f4fe60dd025bfc9-us3&id=0e109de66a"//API KEY  
+               +  "&email[email]=" + email //email address registering
+               +  "&merge_vars[NAME]=" + name //name we're registering
+               +  "&merge_vars[MESSAGE]=" + message
+               +  "&double_optin=false" //add to list even if email is invalid
+               +  "&send_welcome=false"; //send an email notification to new subscriber
+   $.ajax({
+         type: "POST",
+         url: url,
+         dataType: 'json',
+         success: function(data) {
+           $('#subscribe-form').html("Thank you!");
+         },
+         error: function(jqXHR, textStatus, errorThrown) {
+           alert(errorThrown);
+         }
+     });//EO ajax
+ });//EO click bind
     
 })
