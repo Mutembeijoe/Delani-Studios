5f888398117d89ed75b6d3038d6a4695a8a6f40b
mutembeijoe <mutembeijoe@gmail.com>
2019-08-24 01:52:58 +0300

    added a slider for personal brand, rearranged code to be more neat

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
         $(this).children('.show-case-img').stop().fadeTo("fast",1)
         $(this).children('.portfolio-desc').css({opacity:0})
     });
-    
+
     //  smooth scroll behaviour in jquery
    $('a[href="#about"]').click(function(e){
        if(this.hash != ""){
@@ -36,39 +36,46 @@ $(document).ready(function(){
        }
    })
 
-/*  Mailchimp Subscribe API */
-$('#subscribe-form').bind('submit', function(event) {
-    event.preventDefault(); 
-    var name = $('[name="client_name"]').val();
-    var email = $('[name="client_email"]').val();
-    var message = $('[name="client_message"]').val();
-    var url =   "https://us3.api.mailchimp.com/2.0/lists/subscribe.json?"
-               +  "apikey=d8ebe42f1475d69123eee37ca1cb78ba-us3&id=0e109de66a"//API KEY  
-               +  "&email[email]=" + email 
-               +  "&merge_vars[NAME]=" + name 
-               +  "&merge_vars[MESSAGE]=" + message
-               +  "&double_optin=false" //add to list even if email is invalid
-               +  "&send_welcome=false"; //prevent send an email notification to new subscriber
-   $.ajax({
-        //  type: "POST",
-        url: url,
-        crossDomain:true,
-        dataType: 'jsonp',
-        success: function(data) {
-        //    $('#subscribe-form').html("Thank you!");
-        $('form').prepend(`<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Hi, ${name} we received your mail</div>`);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-        //    alert(errorThrown);
-        $('form').prepend(`<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Hi, ${name} we received your mail</div>`);
+    /*  Mailchimp Subscribe API */
+    $('#subscribe-form').bind('submit', function(event) {
+        event.preventDefault(); 
+        var name = $('[name="client_name"]').val();
+        var email = $('[name="client_email"]').val();
+        var message = $('[name="client_message"]').val();
+        var url =   "https://us3.api.mailchimp.com/2.0/lists/subscribe.json?"
+                +  "apikey=d8ebe42f1475d69123eee37ca1cb78ba-us3&id=0e109de66a"//API KEY  
+                +  "&email[email]=" + email 
+                +  "&merge_vars[NAME]=" + name 
+                +  "&merge_vars[MESSAGE]=" + message
+                +  "&double_optin=false" //add to list even if email is invalid
+                +  "&send_welcome=false"; //prevent send an email notification to new subscriber
+    $.ajax({
+            //  type: "POST",
+            url: url,
+            crossDomain:true,
+            dataType: 'jsonp',
+            success: function(data) {
+            //    $('#subscribe-form').html("Thank you!");
+            $('form').prepend(`<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Hi, ${name} we received your mail</div>`);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+            //    alert(errorThrown);
+            $('form').prepend(`<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Hi, ${name} we received your mail</div>`);
 
-        }
+            }
+        });
+
+        $('input[name="client_email"')[0].value = "";
+        $('input[name="client_name"')[0].value = "";
+        $('textarea')[0].value = "";
+
     });
-
-    $('input[name="client_email"')[0].value = "";
-    $('input[name="client_name"')[0].value = "";
-    $('textarea')[0].value = "";
-
- });
     
+    // Additional personal brand
+    $('#personal-brand').hover(function(){
+        $(this).stop().animate({left:'-20px'})
+    },function(){
+        $(this).stop().animate({left:'-180px'})
+    })
+        
 })
